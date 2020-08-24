7b09c40229e81af16e875f1c10e59fd6eebe5c16
PraveenSundar31 <55100749+PraveenSundar31@users.noreply.github.com>
2020-08-24 12:56:04 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,61 @@
+var info = new Date();
+var day=info.getDay();
+if (day==1) {
+    if (confirm("Today is Monday....If you want to see what's in the Webpage click OK ")) {
+        window.opener=self; 
+    }
+    else{
+        window.open('','_self','');
+        window.close();
+    }
+}
+
+/*if (day==1) {
+if (confirm("If you want to directly attend the meeting, click OK. It will be redirected. If you want to visit the Webpage click Cancel. ")) {
+    open("mon link"); 
+    }
+    else{
+        window.opener=self;
+    }
+}*/
+/*if (day==2) {
+if (confirm("If you want to directly attend the meeting, click OK. It will be redirected. If you want to visit the Webpage click Cancel. ")) {
+    open("tue link"); 
+    }
+    else{
+        window.opener=self;
+    }
+}*/
+/*if (day==3) {
+if (confirm("If you want to directly attend the meeting, click OK. It will be redirected. If you want to visit the Webpage click Cancel. ")) {
+    open("wed link"); 
+    }
+    else{
+        window.opener=self;
+    }
+}*/
+/*if (day==4) {
+if (confirm("If you want to directly attend the meeting, click OK. It will be redirected. If you want to visit the Webpage click Cancel. ")) {
+    open("thu link"); 
+    }
+    else{
+        window.opener=self;
+    }
+}*/
+/*if (day==5) {
+if (confirm("If you want to directly attend the meeting, click OK. It will be redirected. If you want to visit the Webpage click Cancel. ")) {
+    open("fri link"); 
+    }
+    else{
+        window.opener=self;
+    }
+}*/
+/*if (day==6) {
+    if (confirm("If you want to directly attend the meeting, click OK. It will be redirected. If you want to visit the Webpage click Cancel. ")) {
+        open("sat link"); 
+    }
+    else{
+        window.opener=self;
+    }
+}*/
+
