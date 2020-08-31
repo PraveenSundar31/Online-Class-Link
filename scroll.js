08bc4828f03e6139df2d058c13258f7f7288ca33
PraveenSundar31 <55100749+PraveenSundar31@users.noreply.github.com>
2020-08-31 23:33:14 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,11 @@
+function abc() {
+    var elmnt = document.getElementById("button");
+    elmnt.scrollIntoView();
+    
+}
+
+function xyz() {
+    $(document).ready(function(){
+        $(this).scrollTop(0);
+    });
+}
